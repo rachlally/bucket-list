147e8336da87c739a1e0e7aaf866d96ac3055fd0
James Pace <pacej2022@gmail.com>
2022-10-11 12:59:03 -0700

    updating html file

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,3 +1,17 @@
-console.log("linked")
-$('#textarea1').val('New Text');
-M.textareaAutoResize($('#textarea1'));
+var url = `https://developers.pinterest.com/pinterest-api@pinterest.com`
+
+function getcords(event){
+    event.preventDefault()
+    console.log(seachedcity.value)
+var city = seachedcity.value
+var url = `https://api.openweathermap.org/data/2.5/weather?q=${city}&appid=b6a175dbcda214328767ec5bb50e19a1`
+
+fetch(url).then(response=>{
+    console.log(response)
+    return response.json()
+}) .then(data =>{
+    
+    console.log(data)
+    cityname.textContent = data.name
+})
+}   
