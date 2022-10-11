70f89ca80f153e153ee9fc19976a693f8b8f0fe7
Dahlia <rlally89@gmail.com>
2022-10-11 16:25:27 -0700

    added bored api, see console log

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,21 @@
 console.log("linked")
-
 $('#textarea1').val('New Text');
 M.textareaAutoResize($('#textarea1'));
 
+
+
+function getApi() {
+    var requestUrl = 'https://www.boredapi.com/api/activity';
+
+    fetch(requestUrl)
+        .then(function(response) {
+        return response.json();
+
+    })
+    .then(function(data) {
+        console.log(data)
+    })
+
+
+}
+getApi()
