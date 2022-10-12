b3f59e33d69fa6ad408c1349cf55dfe47e09bd80
Dahlia <rlally89@gmail.com>
2022-10-12 08:17:13 -0700

    TrueWay Places API added, fetch success

--- assets/js/script.js
+++ assets/js/script.js
@@ -14,8 +14,33 @@ function getApi() {
     })
     .then(function(data) {
         console.log(data)
+        console.log(data.activity);
     })
 
 
 }
 getApi()
+
+const options = {
+	method: 'GET',
+	headers: {
+		'X-RapidAPI-Key': 'ec9e8aa11bmsh01afb5356f556dep1d5473jsn3185f0830ed0',
+		'X-RapidAPI-Host': 'trueway-places.p.rapidapi.com'
+	}
+};
+
+var locationString = "The Showbox";
+
+fetch("https://trueway-places.p.rapidapi.com/FindPlaceByText?text=" + locationString + "&language=en", options)
+    .then(function(response) {
+    return response.json();
+
+})
+    .then(function(data) {
+    console.log(data)
+    console.log(data.results[0].name);
+    console.log(data.results[0].website);
+})
+
+
+
