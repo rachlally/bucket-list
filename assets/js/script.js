b1b8f3a5b9a641e17c8feda7775f02a2d8963041
James Pace <pacej2022@gmail.com>
2022-10-12 09:26:40 -0700

    updated jmp branch

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,5 +1,22 @@
-console.log("linked")
+var btn = document.getElementById("btn")
+var activity = document.getElementById("activity")
 
-$('#textarea1').val('New Text');
-M.textareaAutoResize($('#textarea1'));
+function getApi(event) {
+    console.log(activity.value)
+    var requestUrl = 'https://www.boredapi.com/api/activity';
 
+    fetch(requestUrl)
+        .then(function(response) {
+        return response.json();
+
+    })
+    .then(function(data) {
+        console.log(data)
+        activity.textContent = data.activity
+    })
+
+
+}
+getApi()
+btn.addEventListener("click", getApi)
+git 
