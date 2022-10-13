7c05b6a2cc81e12cd2196e5e3c81916badef5097
swnova <sharrodwillanova@gmail.com>
2022-10-13 13:58:36 -0700

    function for google map

--- assets/js/script.js
+++ assets/js/script.js
@@ -5,7 +5,60 @@ var savedBucketItem = $('#firstActivityButton');
 var activityHistory = [];
 //Save activity input from user to local storage
 
+// Google Maps APi
+//AIzaSyD7wWUU2e0wjZ__Ds9BAWzuFFU19QhHLMI
 
+var map;
+function initMap(){
+    var pointer = new google.maps.LatLng(47.60, 122.33);
+
+var map= new google.maps.Map(document.getElementById('map'),{
+            
+    infowindow = new google.maps.InfoWindow();       
+    
+
+});
+    }
+
+// {
+//     address: string,
+//     location: LatLng,
+//     placeId: string,
+//     bounds: LatLngBounds,
+//     componentRestrictions: GeocoderComponentRestrictions,
+//     region: string
+//    }
+
+// initialize map
+
+// var map;
+// var service;
+// var infowindow;
+
+// function initMap() {
+//   var sydney = new google.maps.LatLng(-33.867, 151.195);
+
+//   infowindow = new google.maps.InfoWindow();
+
+//   map = new google.maps.Map(
+//       document.getElementById('map'), {center: sydney, zoom: 15});
+
+//   var request = {
+//     query: 'Museum of Contemporary Art Australia',
+//     fields: ['name', 'geometry'],
+//   };
+
+//   var service = new google.maps.places.PlacesService(map);
+
+//   service.findPlaceFromQuery(request, function(results, status) {
+//     if (status === google.maps.places.PlacesServiceStatus.OK) {
+//       for (var i = 0; i < results.length; i++) {
+//         createMarker(results[i]);
+//       }
+//       map.setCenter(results[0].geometry.location);
+//     }
+//   });
+// }
 
 savedBucketItem.on("click", function () {
     var bucketActivity1 = $("#freeform");
