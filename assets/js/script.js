832f3c4b77e18aea952479d6f53dfa0823007ae2
James Pace <pacej2022@gmail.com>
2022-10-14 11:04:01 -0700

    adding to local

--- assets/js/script.js
+++ assets/js/script.js
@@ -111,27 +111,6 @@ function getApi(event) {
  //Fill My Bucket button to generate event for user
  btn.addEventListener("click", getApi)
  
- 
-// const options = {
-//       method: 'GET',
-//       headers: {
-//           'X-RapidAPI-Key': 'ec9e8aa11bmsh01afb5356f556dep1d5473jsn3185f0830ed0',
-//           'X-RapidAPI-Host': 'trueway-places.p.rapidapi.com'
-//       }
-//   };
- 
-//   var locationString = "The Showbox";
- 
-//   fetch("https://trueway-places.p.rapidapi.com/FindPlaceByText?text=" + locationString + "&language=en", options)
-//       .then(function(response) {
-//       return response.json();
- 
-//   })
-//       .then(function(data) {
-//       console.log(data)
-//       console.log(data.results[0].name);
-//       console.log(data.results[0].website);
-//   })
 
 // document.querySelector("#files").addEventListener("change", (e) => {
 //     if(window.File && window.FileReader && window.FileList && window.Blob){
